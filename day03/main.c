0f9f3b66829b1d4d68b689b106667fae72614708
Lukas <glukas94@gmail.com>
2021-12-03 21:11:06 +0100

    stil trying out

--- day03/main.c
+++ day03/main.c
@@ -6,7 +6,7 @@
 /*   By: lugonzal <lugonzal@student.42urduli>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/02 13:56:20 by lugonzal          #+#    #+#             */
-/*   Updated: 2021/12/03 16:32:59 by lugonzal         ###   ########.fr       */
+/*   Updated: 2021/12/03 21:10:39 by lugonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,65 +17,90 @@
 #include "../libft/inc/libft.h"
 #include "../libft/inc/get_next_line.h"
 
-static void	ft_check_binary(int bit_rate[12][2], char *line)
+static void	free_d2(char **data)
 {
-	size_t	j;
+	size_t	i;
 
-	j = 0;	
-	while (line[j] && line[j] != 10)
-	{
-		line[j] -= 48;
-		if (line[j])
-			bit_rate[j][1]++;
-		else
-			bit_rate[j][0]++;
-		j++;
-	}
+	i = -1;
+	while (data[++i])
+		free(data[i]);
+	free(data);
 }
 
-static char	**ft_binary_result(int bit_rate[12][2])
+static char	**ft_occurrence(void)
 {
-	static char	*epsilon[2];
-	size_t		j;
+	int		fd;
+	char	**info;
+	size_t	i;
 
-	j = -1;
-	epsilon[0] = ft_calloc(sizeof(char), 13);
-	epsilon[1] = ft_calloc(sizeof(char), 13);
-	while (++j < 12)
+	i = 0;
+	info = ft_calloc(sizeof(char *), 10001);
+	fd = open("input5", O_RDONLY);
+	while (1)
 	{
-		if (bit_rate[j][1] > bit_rate[j][0])
-		{
-			epsilon[0][j] = '1';	
-			epsilon[1][j] = '0';	
-		}
-		else
-		{
-			epsilon[0][j] = '0';	
-			epsilon[1][j] = '1';	
-		}
+		info[i] = get_next_line(fd);
+		if (!info[i])
+			break ;
+		info[i][6] = 0;
+		i++;
 	}
-	return (epsilon);
+	return (info);
+}
+
+static char **ft_resize(char **binary_info, char c, size_t pos, size_t *size, size_t old_size)
+{
+	char	**binary;
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = -1;
+	binary = ft_calloc(sizeof(char *), size + 1);
+	while (i < size)
+	{
+		while (++j < old_size)
+		{
+			if (binary_info[j][pos] == c)
+				break ;
+		}
+		binary[i] = ft_strdup(binary_info[j]);
+		i++;
+	}
+	(*size) = i;
+	free_d2(binary_info);
+	return (binary);
+}
+
+static char **ft_result(char **binary_info, size_t pos, size_t size)
+{
+	static int	binary[2] = {0};
+	size_t		i;
+
+	i = -1;
+	while (++i < size)
+	{
+		if (binary_info[i][pos] - 48)
+			binary[1]++;
+		else
+			binary[0]++;
+	}
+	if (binary[1] >= binary[0])
+	{
+		binary_info = ft_resize(binary_info, '1', pos, binary[1], size);
+		if (size != 1)
+			ft_result(binary_info, ++pos, 
+	}
+	else
+		binary_info = ft_resize(binary_info, '0', pos, binary[0], size);
+	return (binary_info);
 }
 
 int	main(void)
-{	
-	char		*line;
-	int			fd;
-	static int	bit_rate[12][2];
-	char		**result;
+{
+	char	**binary_info;
 
-	fd = open("1", O_RDONLY);
-	while (1)
-	{
-		line = get_next_line(fd);
-		if (!line)
-			break ;
-		ft_check_binary(bit_rate, line);
-		free(line);
-	}
-	result = ft_binary_result(bit_rate);
-	printf("RESULT_1: %s\n", result[0]);
-	printf("RESULT_2: %s", result[1]);
-	free(result[0]);
-	free(result[1]);
+	binary_info = ft_occurrence();
+	binary_info = ft_result(binary_info, 0, 12);
+	printf("%s", binary_info[0]);
+	printf("%s", binary_info[1]);
 }
