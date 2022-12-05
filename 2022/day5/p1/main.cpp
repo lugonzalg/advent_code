e1261a97fe521a5ebae5e5597d34ae64cef84e8a
Lukas Gonzalez <lukas@techfriendly.es>
2022-12-05 19:41:17 +0100

    p1

---
+++ 2022/day5/p1/main.cpp
@@ -0,0 +1,54 @@
+#include <string.h>
+
+#include <vector>
+#include <iostream>
+#include <fstream>
+
+static void	handle_vector(int i, int from, int to, std::vector<char> v[3])
+{
+	char	tmp;
+	for (; i; i--)
+	{
+		tmp = v[from].front();
+		v[from].erase(v[from].begin());
+		v[to].insert(v[to].begin(), tmp);
+	}
+}
+
+int main(void)
+{
+	std::vector<char> v[10] = {{}, {'V','N','F', 'S', 'M', 'P', 'H', 'J'},{'Q','D','J', 'M', 'L', 'R', 'S'},{'B', 'W', 'S', 'C', 'H', 'D', 'Q', 'N'}, {'L', 'C', 'S', 'R'}, {'B', 'F', 'P', 'T', 'V', 'M'}, {'C', 'N', 'Q', 'R', 'T'}, {'R', 'V', 'G'}, {'R', 'L', 'D', 'P', 'S', 'Z', 'C'}, {'F', 'B', 'P', 'G', 'V', 'J', 'S', 'D'}};
+	//std::ifstream	in("test_output.txt");
+	std::ifstream	in("output.txt");
+	char	buff[100];
+	int		num_set[3];
+	int		j;
+
+	while (1)
+	{
+		memset(buff, 0, 100);
+		in.getline(buff, 100);
+		j = 0;
+		memset(&num_set, 0, sizeof(int) * 3);
+		for (size_t i = 0; i < strlen(buff); i++)
+		{
+			while (!isdigit(buff[i]))
+					i++;
+			while (isdigit(buff[i]))
+			{
+				num_set[j] = num_set[j] * 10 + buff[i] - 48;
+				i++;
+			}
+			j++;
+		}
+		handle_vector(num_set[0], num_set[1], num_set[2], v);
+		if (in.peek() == EOF)
+			break ;
+	}
+	for (int i = 1; i < 10; i++)
+	{
+		std::cout << "[" << i << "]: " << v[i][0];
+	}
+
+	return (0);
+}
